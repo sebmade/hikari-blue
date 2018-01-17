b659640f48197c3be242f9b3958d672db1f43d4a
ntrelat <ntrelat@actweb.fr>
2018-01-17 17:37:27 +0100

    inté

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -3,4 +3,16 @@
 
 'use strict';
 
+var jQuery = require('jquery');
 require('material-design-lite');
+
+(function($){
+  $(window).on('scroll', function(){
+    if($(window).scrollTop() > 10 ){
+      $('.mdl-layout__header').addClass('mini');
+    }
+    else{
+      $('.mdl-layout__header').removeClass('mini');
+    }
+  });
+})(jQuery);
